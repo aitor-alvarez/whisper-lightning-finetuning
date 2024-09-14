448f3b22113be1fc85b95ee411e28204f98d8fbe
aitor-arronte <arronte@hawaii.edu>
2024-09-13 23:02:31 -1000

    exec code

--- loader.py
+++ loader.py
@@ -28,8 +28,8 @@ def load_data(dir=None, dataset=None, data_lang=None, is_local=False):
         train = load_dataset("audiofolder", data_dir=dir, split="train")
         test = load_dataset("audiofolder", data_dir=dir, split="test")
     else:
-        train = load_dataset(dataset, data_lang, split="train", streaming=True)
-        test = load_dataset(dataset, data_lang, split="test", streaming=True)
+        train = load_dataset(dataset, data_lang, split="train", streaming=True, trust_remote_code=True)
+        test = load_dataset(dataset, data_lang, split="test", streaming=True, trust_remote_code=True)
 
     if not os.path.exists('vocab.json'):
         extract_vocab(train, test)
